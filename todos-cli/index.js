ddc70e5e5b665c4f9abc91f163ac6f68e5eb5a93
Zaker Safdari <zakersafdari@gmail.com>
2024-12-13 22:53:59 +1300

    created the add command

--- todos-cli/index.js
+++ todos-cli/index.js
@@ -9,4 +9,9 @@ program
     .description("List all the TODO tasks")
     .action(list)
 
+program
+    .command("add <task>")
+    .description("Add a new task to the TODO list")
+    .action(add)    
+
 program.parse();
