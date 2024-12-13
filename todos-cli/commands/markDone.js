ec3d23a9b20665e547307d6ac82b42a0cc8b18c3
Zaker Safdari <zakersafdari@gmail.com>
2024-12-13 23:35:28 +1300

    implemented the markDone function and take into account the option scenario

---
+++ todos-cli/commands/markDone.js
@@ -0,0 +1,31 @@
+import Conf from "conf";
+import chalk from "chalk";
+
+const conf = new Conf();
+
+export default function markDone({ tasks }) {
+    let todoList = conf.get("todoList")
+
+    if(todoList) {
+        //loop over the todo list tasks
+        todoList = todoList.map((task, index) => {
+            //check if the user specified the tasks to mark as done
+            if (tasks) {
+                //check if this task is one of the tasks the user specified
+                if (tasks.indexOf(index.toString()) !== -1) {
+                    //mark only specified tasks by user as done
+                    task.done = true
+                }
+            } else {
+                //if the user didn't specify tasks, mark all tasks as done
+                task.done = true
+            }
+            return task
+        })
+        //set the new todo-list
+        conf.set("todo-List", todoList)
+    }
+    //show the user a message
+    console.log(chalk.green.bold("Tasks have been marked as done as successfully"))
+
+}
