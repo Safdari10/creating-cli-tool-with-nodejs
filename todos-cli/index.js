c5164880fed6ad8c9c363abd80834cdf31115842
Zaker Safdari <zakersafdari@gmail.com>
2024-12-13 23:36:07 +1300

    import markDone command in the main application file

--- todos-cli/index.js
+++ todos-cli/index.js
@@ -3,6 +3,7 @@
 import { program } from 'commander';
 import list from './commands/list.js';
 import add from './commands/add.js';
+import markDone from './commands/markDone.js';
 
 program
     .command("list")
